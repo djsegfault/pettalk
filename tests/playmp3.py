7d553a41536131238eaa02dc1f7df047a0339ed7
david.kramer <david@thekramers.net>
2019-11-15 03:10:19 -0500

    now load sound files into an array

--- tests/playmp3.py
+++ tests/playmp3.py
@@ -4,11 +4,24 @@ import sys
 from pydub import AudioSegment
 from pydub.playback import play
 
+# Load songs into array
 soundFilePath="../audio/"
+soundFileTemplate=soundFilePath + "{}_{}.mp3"
 
-buttonPosition = sys.argv[1] + "_" + sys.argv[2]
-soundFile=soundFilePath + buttonPosition + ".mp3"
-print("Playing sound " + soundFile)
+soundClips = [[0,0,0],[0,0,0]]
+for row in [0, 1]:
+    for col in [0, 1, 2]:
+        soundFile=soundFileTemplate.format(row, col)
+        print("Loading " + soundFile)
+        soundClips[row][col] = AudioSegment.from_mp3(soundFile)
+        
 
-song = AudioSegment.from_mp3(soundFile)
-play(song)
+
+row = int(sys.argv[1])
+col = int(sys.argv[2])
+#soundFile=soundFilePath + buttonPosition + ".mp3"
+
+print("Playing sound {} {} ".format(row, col))
+
+clip=soundClips[row][col] 
+play(clip)
