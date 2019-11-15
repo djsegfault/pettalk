8ed30a1f511e28d8f1b4adc2124cc219d1906b12
david.kramer <david@thekramers.net>
2019-11-15 03:19:37 -0500

    Check file exists first

--- tests/playmp3.py
+++ tests/playmp3.py
@@ -4,6 +4,9 @@ import sys
 from pydub import AudioSegment
 from pydub.playback import play
 
+import os.path
+from os import path
+
 # Load songs into array
 soundFilePath="../audio/"
 soundFileTemplate=soundFilePath + "{}_{}.mp3"
@@ -12,11 +15,12 @@ soundClips = [[0,0,0],[0,0,0]]
 for row in [0, 1]:
     for col in [0, 1, 2]:
         soundFile=soundFileTemplate.format(row, col)
-        print("Loading " + soundFile)
-        soundClips[row][col] = AudioSegment.from_mp3(soundFile)
+        if path.exists(soundFile):
+            print("Loading " + soundFile)
+            soundClips[row][col] = AudioSegment.from_mp3(soundFile)
+        else:
+            print("WARNING: {} is missing!".format(soundFile))
         
-
-
 row = int(sys.argv[1])
 col = int(sys.argv[2])
 #soundFile=soundFilePath + buttonPosition + ".mp3"
